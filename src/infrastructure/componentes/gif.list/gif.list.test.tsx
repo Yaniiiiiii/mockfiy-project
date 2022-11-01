71864c8365e99b82018435d492694a49ee3ba9ba
marcos <marcos.bonet@live.com.ar>
2022-11-01 17:00:06 +0100

    update test giflist

---
+++ src/infrastructure/componentes/gif.list/gif.list.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { IElementData } from '../../models/data';
+import { Giflist } from './gif.list';
+
+describe('Given GifList component', () => {
+    describe('When we render the component', () => {
+        test('then it should display the Gifs List', () => {
+            const mockGifList: Array<IElementData> = [
+                {
+                    title: 'test',
+                    images: {
+                        original: {
+                            url: '',
+                        },
+                    },
+                },
+            ];
+            render(<Giflist data={mockGifList} />);
+            const element = screen.getByAltText(/test/i);
+            expect(element).toBeInTheDocument();
+        });
+    });
+});
