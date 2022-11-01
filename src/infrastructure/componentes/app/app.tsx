d3d5b95dc47359f207cd5c7eb066968eb9d535c7
luisherreralg <lherreralg@outlook.es>
2022-11-01 17:22:25 +0100

    Updated reducer

--- src/infrastructure/componentes/app/app.tsx
+++ src/infrastructure/componentes/app/app.tsx
@@ -1,8 +1,42 @@
-import { getData, getSearchData } from '../../services/gifs.api';
+import { useEffect, useState } from 'react';
+import { getSearchData } from '../../services/gifs.api';
 import './app.css';
+import * as actions from '../../reducers/action.creator';
 
 export function App() {
-    getData();
-    getSearchData('cactus');
-    return <div className="app">Probando</div>;
+    const [Search, setSearchState] = useState({
+        type: '',
+        payload: {
+            data: [
+                {
+                    title: ``,
+                    images: {
+                        original: {
+                            url: '',
+                        },
+                    },
+                },
+            ],
+            pagination: {
+                count: 0,
+                offset: 0,
+            },
+        },
+    });
+
+    useEffect(() => {
+        getSearchData('Testing').then((resps) => {
+            setSearchState(actions.loadGifAction(resps));
+        });
+    }, []);
+
+    console.log(Search);
+
+    return (
+        <div className="app">
+            {Search.payload.data.map((item) => (
+                <img src={item.images.original.url} alt="" />
+            ))}
+        </div>
+    );
 }
