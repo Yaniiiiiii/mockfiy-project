856da27c36d62b0592cb13d20975f9612d703e2a
Yanira <yanirazapata.c@gmail.com>
2022-11-01 12:30:50 +0100

    create item test

---
+++ src/infrastructure/componentes/gifItem/gifItem.test.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { GifItem } from './gifItem';
+
+describe('Given the gifItem component', () => {
+    describe('When we render the component', () => {
+        test('then it should display the item´s gif', () => {
+            const mockItem = {
+                image: {
+                    original: {
+                        url: 'https://media4.giphy.com/media/mi6DsSSNKDbUY/giphy.gif?cid=4e847dbcoiwns7ccq97ezwn4dc6y3qt2qh6zkq5me7u36eqq&rid=giphy.gif&ct=g',
+                    },
+                },
+            };
+
+            render(<GifItem item={mockItem} />);
+        });
+    });
+});
