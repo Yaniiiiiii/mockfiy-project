c14ab11b43e107cc9064eb612de07960851bb1c8
luisherreralg <lherreralg@outlook.es>
2022-11-03 20:45:31 +0100

    Fixed a code smell

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './infrastructure/componentes/app/app';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
