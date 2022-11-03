b0cd84837ea205fc44212b69e530827ed7b53db7
luisherreralg <lherreralg@outlook.es>
2022-11-03 18:33:15 +0100

    Updated test

--- src/infrastructure/hook/use.gif.test.tsx
+++ src/infrastructure/hook/use.gif.test.tsx
@@ -51,7 +51,7 @@ describe('Given the custom hook "useGif" component', () => {
         });
     });
 
-    test('When we use the handleAdd, then it should return the "newGif" object', async () => {
+    test('When we use the handleAdd, then it should return the "newGif" object and been called', async () => {
         await waitFor(() => {
             result.current.handleAdd(newGif);
             expect(result.current.localGif.at(-1)).toEqual(newGif);
@@ -61,21 +61,27 @@ describe('Given the custom hook "useGif" component', () => {
         });
     });
 
-    test('When we use handleUpdate then it should return the "newGif" object updated', async () => {
+    test('When we use handleUpdate then it should return the "newGif" object updated adn been called', async () => {
         await waitFor(() => {
             result.current.handleUpdate(newGif);
             expect(result.current.localGif.at(-1)).toEqual(newGif);
         });
+        await waitFor(() => {
+            expect(apiCalls.updateLocalData).toHaveBeenCalled();
+        });
     });
 
-    test('When we use handleUpdate then it should return the "gif1" object updated', async () => {
+    test('When we use handleEraser and the response is ok then it should return the same object and been called', async () => {
         await waitFor(() => {
             result.current.handleEraser(gif1);
             expect(result.current.localGif.at(-1)).toEqual(gif1);
         });
+        await waitFor(() => {
+            expect(apiCalls.deleteLocalData).toHaveBeenCalled();
+        });
     });
 
-    test('When we use handleUpdate then it should return the "gif1" object update', async () => {
+    test('When we use handleEraser and the response is not ok then it should return nothing but it must be called', async () => {
         (apiCalls.deleteLocalData as jest.Mock).mockResolvedValue({
             ok: true,
         });
