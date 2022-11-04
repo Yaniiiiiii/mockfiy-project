1c56b554c398547f418b37e1ae17c507458fd467
Yanira <yanirazapata.c@gmail.com>
2022-11-04 17:39:38 +0100

    create fav button

--- src/infrastructure/componentes/gifItem/gifItem.tsx
+++ src/infrastructure/componentes/gifItem/gifItem.tsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
+import { GifContext } from '../../context/context';
 import { IElementData } from '../../models/data';
 
 export function GifItem({ item }: { item: IElementData }) {
+    const { handleAdd } = useContext(GifContext);
+    const handleClickAdd = () => {
+        handleAdd(item);
+    };
     return (
         <li>
             <img src={item.images.original.url} alt={'gif' + item.title}></img>
+            <button className={`♡`} onClick={handleClickAdd}></button>
         </li>
     );
 }
