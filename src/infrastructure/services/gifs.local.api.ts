3cd6c8caa1257c97761b2b3b06da1a2077484cab
luisherreralg <lherreralg@outlook.es>
2022-11-07 17:39:49 +0100

    updated localhost url

--- src/infrastructure/services/gifs.local.api.ts
+++ src/infrastructure/services/gifs.local.api.ts
@@ -1,21 +1,28 @@
 import { IElementData } from '../models/data';
 
 export const getLocalData = async (): Promise<Array<IElementData>> => {
-    const url = 'http://localhost:3500/data';
-    const result = await fetch(url).then((response) => {
-        if (response.ok) return response.json();
-        const message = `Error ${response.status}: ${response.statusText}`;
-        const error = new Error(message);
-        error.name = 'HTTPError';
-        throw error;
-    });
+    const url =
+        'https://mockify-202210-madserver-production.up.railway.app/data';
+    const result = await fetch(url)
+        .then((response) => {
+            if (response.ok) return response.json();
+            const message = `Error ${response.status}: ${response.statusText}`;
+            const error = new Error(message);
+            error.name = 'HTTPError';
+            throw error;
+        })
+        .catch(() => {
+            // Aqui se cambia el estado de has error a true
+            // Por ejemplo
+        });
     return result;
 };
 
 export const createLocalData = async (
     newGif: IElementData
 ): Promise<IElementData> => {
-    const url = 'http://localhost:3500/data';
+    const url =
+        'https://mockify-202210-madserver-production.up.railway.app/data';
     const result = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(newGif),
@@ -33,7 +40,7 @@ export const createLocalData = async (
 };
 
 export const deleteLocalData = async (id: string) => {
-    const url = `http://localhost:3500/data/${id}`;
+    const url = `https://mockify-202210-madserver-production.up.railway.app/data/${id}`;
     const result = await fetch(url, {
         method: 'DELETE',
     }).then((response) => {
@@ -50,7 +57,7 @@ export const updateLocalData = async (
     id: string,
     partialGifData: IElementData
 ) => {
-    const url = `http://localhost:3500/data/${id}`;
+    const url = `https://mockify-202210-madserver-production.up.railway.app/data/${id}`;
     const result = await fetch(url, {
         method: 'PATCH',
         body: JSON.stringify(partialGifData),
